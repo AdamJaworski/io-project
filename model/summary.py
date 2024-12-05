f38ee296733e4be8d6553d5843ee84ae1bdbe34c
AdamJaworski <jaworskiadam0203@gmail.com>
2024-12-05 23:39:09 +0100

    dynamic adjusted contex window

--- model/summary.py
+++ model/summary.py
@@ -1,4 +1,10 @@
 from llama_cpp import Llama
 from common.path_manager import PathManager
 
-llm = Llama(model_path=str(PathManager().model_llama / 'model.gguf'), n_ctx=65536)
+current_n_ctx = 65536
+llm = Llama(model_path=str(PathManager().model_llama / 'model.gguf'), n_ctx=current_n_ctx, verbose=False)
+
+def double_n_ctx():
+    global llm, current_n_ctx
+    current_n_ctx *= 2
+    llm = Llama(model_path=str(PathManager().model_llama / 'model.gguf'), n_ctx=current_n_ctx, verbose=False)
