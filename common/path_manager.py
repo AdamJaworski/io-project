fb015fbf1f019ac783b2d4097459d6666f77bdeb
AdamJaworski <jaworskiadam0203@gmail.com>
2024-12-06 18:45:33 +0100

    Improved PathManager - added option to initialize with report_id, added option to changed reutrn type while calling a function

--- common/path_manager.py
+++ common/path_manager.py
@@ -1,9 +1,11 @@
 import os.path
 from pathlib import Path
-
+from typing import Optional
 
 class PathManager:
-    def __init__(self):
+    def __init__(self, report_id: Optional[str] = None):
+        self.report_id = report_id
+
         self.main_catalog = Path().resolve()
         while str(self.main_catalog).split('\\')[-1] != 'io-project':
             self.main_catalog = self.main_catalog.parent.resolve()
@@ -16,10 +18,13 @@ class PathManager:
     def create_dirs(self):
         self.outputs.mkdir(exist_ok=True, parents=True)
 
-    def get_report_path(self, report_id: str) -> Path:
+    def get_report_path(self, report_id: Optional[str] = None, return_type: Optional[type] = Path) -> Path:
+        assert report_id or self.report_id, 'You need to provide report id'
+        report_id = report_id if report_id else self.report_id
+
         self.report_path = (self.outputs / f"{report_id}")
         self.report_path.mkdir(exist_ok=True, parents=True)
-        return self.report_path
+        return return_type(self.report_path)
 
 if __name__ == "__main__":
     PathManager()
